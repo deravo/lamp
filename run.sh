42f1b80a3076e27fd38c3bfbb8db8f4aa3d2778c
deravo <tenkuking@163.com>
2015-09-15 19:44:19 +0800

    reload supervisor

--- run.sh
+++ run.sh
@@ -8,11 +8,11 @@ VOLUME_HOME="/var/lib/mysql"
 
 sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
     -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
-if [[ ! -d $VOLUME_HOME ]]; then
+if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
     echo "=> Installing MySQL ..."
     mysql_install_db > /dev/null 2>&1
-    echo "=> Done!"
+    echo "=> Done!"  
     /create_mysql_admin_user.sh
 else
     echo "=> Using an existing volume of MySQL"
@@ -24,17 +24,8 @@ fi
 
 rm -fr /var/lib/apt/lists/*
 
-echo "Starting SSH daemon"
-/usr/sbin/sshd -D
-echo "SSH daemon is running."
-echo ""
+/etc/init.d/ssh start
+/etc/init.d/mysql stop
+/etc/init.d/apache2 stop
 
-echo "restarting MySQL"
-/etc/init.d/mysql restart
-echo "MySQL is running"
-echo ""
-
-echo "restarting apache2"
-/etc/init.d/apache2 restart
-
-echo "Have a nice day"
+exec supervisord -n
